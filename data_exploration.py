a65d47f5dcb161851e53d68614430ef5e709f9dd
OmariMotta <omotta223@gmail.com>
2023-07-20 18:22:40 -0400

    Recent edits

--- data_exploration.py
+++ data_exploration.py
@@ -5,21 +5,30 @@ import seaborn as sns
 import numpy as np
 
 #read the data file
-train_data = pd.read_csv('train.csv')
+df= pd.read_csv('train.csv')
 
 # Prints first few rows
-print(train_data.head())
+print(df.head())
 
 #Prints summary statistics
-print(train_data.describe())
+print(df.describe())
 
 #check for missing data
-print(train_data.isnull().sum())
+print(df.isnull().sum())
 
 #Create Histograms of numeric columns
-train_data.select_dtypes(include=[np.number]).hist(bins=50, figsize=(20,15))
+df.select_dtypes(include=[np.number]).hist(bins=50, figsize=(20,15))
 plt.show()
 
 # Explore Correlations
-corr_matrix = train_data.corr()
+corr_matrix = df.corr()
 print(corr_matrix)
+
+# Heat Map
+sns.heatmap(df.isnull(), yticklabels = False, cbar = False, cmap = 'coolwarm')
+
+# Max Rows and Columns
+np.set_printoptions(threshold = 100000000)
+print(np.arange(10000))
+df.isnull().sum()
+
