fe604ff5afd653d06277ec652f1798325b1e192e
OmariMotta <omotta223@gmail.com>
2023-07-21 18:14:22 -0400

    Co-authored-by: sylaschacko <sylaschacko@users.noreply.github.com>

--- data_exploration.py
+++ data_exploration.py
@@ -32,3 +32,36 @@ np.set_printoptions(threshold = 100000000)
 print(np.arange(10000))
 df.isnull().sum()
 
+# Prints (number of data points, number of columns)
+df.shape
+
+# Prints All columns , number of data points, "non-null", variable type
+df.info() 
+
+#Fill in Missing Values
+df['LotFrontage']=df['LotFrontage'].fillna(df['LotFrontage'].mean())
+df['BsmtCond']=df['BsmtCond'].fillna(df['BsmtCond'].mode()[0])
+df['BsmtQual']=df['BsmtQual'].fillna(df['BsmtQual'].mode()[0])
+df['FireplaceQu']=df['FireplaceQu'].fillna(df['FireplaceQu'].mode()[0])
+df['GarageType']=df['GarageType'].fillna(df['GarageType'].mode()[0])
+df.drop(['GarageYrBlt'],axis=1,inplace=True)
+df['GarageFinish']=df['GarageFinish'].fillna(df['GarageFinish'].mode()[0])
+df['GarageQual']=df['GarageQual'].fillna(df['GarageQual'].mode()[0])
+df['GarageCond']=df['GarageCond'].fillna(df['GarageCond'].mode()[0])
+df.drop(['PoolQC','Fence','MiscFeature','Alley','Id'],axis=1,inplace=True)
+
+df.shape 
+
+df.isnull().sum
+
+#Fills rest of missing values
+df['MasVnrType']=df['MasVnrType'].fillna(df['MasVnrType'].mode()[0])
+df['MasVnrArea']=df['MasVnrArea'].fillna(df['MasVnrArea'].mode()[0])
+df['BsmtExposure']=df['BsmtExposure'].fillna(df['BsmtExposure'].mode()[0])
+
+sns.heatmap(df.isnull(),yticklabels=False,cbar=False,cmap='YlGnBu')
+
+df['Electrical']=df['Electrical'].fillna(df['Electrical'].mean())
+
+## Fill Missing Values
+
